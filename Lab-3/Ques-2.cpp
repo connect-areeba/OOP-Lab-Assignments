f58cb5dc5288dc9c3ec9eb7d8a9cb1bfa58e1f71
Areeba <connect.areeba@gmail.com>
2026-02-07 07:10:57 +0500

    Create tollbooth class to track cars and revenue
    Implemented a tollbooth class with functionality to track cars and money collected.

---
+++ Lab-3/Ques-2.cpp
@@ -0,0 +1,34 @@
+/*2. Create a class tollbooth. The two data items are a type int to hold the total number of cars and a type double to hold the total amount of money collected. A
+constructor initializes both these to 0. When a car passes the toll, a member function called payingCar( ) increments the car total and adds 0.50 to the cash
+total. Another member function displays the two totals. DESIGN and IMPLEMENT
+this case. Make assumptions (if required) and include it in the description before designing the solution.*/
+#include<iostream>
+using namespace std;
+
+class tollbooth{
+    int cars;
+    double money;
+    public:
+        tollbooth(){
+            cars=0;
+            money=0.0;
+        }
+
+        void payingCar(){
+            cars++;
+            money+=0.50;
+        }
+
+        void display(){
+            cout<<"Total cars: "<<cars<<endl<<"Total money collected: $"<<money<<endl;
+        }
+};
+
+int main(){
+    tollbooth t;
+    t.payingCar();
+    t.payingCar();
+    t.display();
+    
+    return 0;
+}
